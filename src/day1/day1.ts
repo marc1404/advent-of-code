accd12c8484f8dd3cf32df9ec3c3d3d850d4e1da
Marc Vornetran <marc.vornetran@gmail.com>
2019-12-06 15:58:20 +0100

    Complete part 2 of day 1

--- src/day1/day1.ts
+++ src/day1/day1.ts
@@ -8,15 +8,34 @@ export function day1(): void {
     const input = fs.readFileSync(inputPath, {encoding: 'utf8'});
     const lines = input.split(os.EOL);
 
-    const moduleMasses = lines.map(line => Number.parseInt(line, 10));
+    const moduleMasses: number[] = lines
+        .map(line => Number.parseInt(line, 10))
+        .filter(moduleMass => !Number.isNaN(moduleMass));
 
     const totalFuel = moduleMasses.reduce((totalFuel, moduleMass) => {
-        if (Number.isNaN(moduleMass)) {
-            return totalFuel;
-        }
-
-        return totalFuel + Math.floor(moduleMass / 3) - 2;
+        return totalFuel + calculateFuel(moduleMass);
     }, 0);
 
     consola.info(`Total fuel requirement: ${totalFuel}`);
+
+    const totalFuelWithCompensation = moduleMasses.reduce((totalFuel, moduleMass) => {
+        return totalFuel + calculateFuel(moduleMass, true);
+    }, 0);
+
+    consola.info(`Total fuel requirement with compoensation: ${totalFuelWithCompensation}`);
+}
+
+function calculateFuel(mass: number, compensateFuel: boolean = false): number {
+    let fuel = Math.floor(mass / 3) - 2;
+    fuel = Math.max(fuel, 0);
+
+    if (fuel === 0) {
+        return fuel;
+    }
+
+    if (compensateFuel) {
+        fuel += calculateFuel(fuel, true);
+    }
+
+    return fuel;
 }
