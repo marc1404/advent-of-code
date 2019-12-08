16ae45538687458414b45494a9471f08aa3d5e3c
Marc Vornetran <marc.vornetran@gmail.com>
2019-12-08 15:02:09 +0100

    Add day 5 to index

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import { day1 } from './day1/day1';
 import { day2 } from './day2/day2';
 import { day3 } from './day3/day3';
 import { day4 } from './day4/day4';
+import { day5 } from './day5/day5';
 
 const dayToFunction: Record<string, () => void> = {
     1: day1,
     2: day2,
     3: day3,
-    4: day4
+    4: day4,
+    5: day5
 };
 
 const day = process.argv[2];
