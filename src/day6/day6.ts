09e5b3d7cabe3c4bc1f18acea707fba59932acf0
Marc Vornetran <marc.vornetran@gmail.com>
2019-12-09 19:30:56 +0100

    Add input for day 6

--- src/day6/day6.ts
+++ src/day6/day6.ts
@@ -1,3 +1,15 @@
-export function day6(): void {
+import fs from 'fs';
+import path from 'path';
+import os from 'os';
+import consola from 'consola';
 
+export function day6(): void {
+    consola.info(readInputLines());
+}
+
+function readInputLines(): string[] {
+    const inputPath = path.join(__dirname, 'input.txt');
+    const input = fs.readFileSync(inputPath, {encoding: 'utf8'});
+
+    return input.split(os.EOL);
 }
