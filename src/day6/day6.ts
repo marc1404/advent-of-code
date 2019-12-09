5ab87148fba33be8cf77a8a0f140e6142bf8275a
Marc Vornetran <marc.vornetran@gmail.com>
2019-12-09 20:26:32 +0100

    Complete day 6 part 2

--- src/day6/day6.ts
+++ src/day6/day6.ts
@@ -5,11 +5,16 @@ import consola from 'consola';
 import assert from 'assert';
 
 export function day6(): void {
-    test();
-    puzzle1();
+    const puzzleOrbits = readInputLines();
+    const puzzleOrbitMap = createOrbitMap(puzzleOrbits);
+
+    test1();
+    puzzle1(puzzleOrbits);
+    test2();
+    puzzle2(puzzleOrbitMap);
 }
 
-function test(): void {
+function test1(): void {
     const orbits = [
         'COM)B',
         'B)C',
@@ -29,11 +34,51 @@ function test(): void {
     assert.strictEqual(checksum, 42);
 }
 
-function puzzle1(): void {
-    const orbits = readInputLines();
+function puzzle1(orbits: string[]): void {
     const orbitCountChecksum = calculateOrbitCountChecksum(orbits);
 
     consola.info(`The orbit count checksum is ${orbitCountChecksum}`);
+}
+
+function test2(): void {
+    const orbits = [
+        'COM)B',
+        'B)C',
+        'C)D',
+        'D)E',
+        'E)F',
+        'B)G',
+        'G)H',
+        'D)I',
+        'E)J',
+        'J)K',
+        'K)L',
+        'K)YOU',
+        'I)SAN'
+    ];
+
+    const orbitMap = createOrbitMap(orbits);
+    const youObject = orbitMap.get('YOU') as SpaceObject;
+    const sanObject = orbitMap.get('SAN') as SpaceObject;
+
+    assert.notStrictEqual(youObject, undefined);
+    assert.notStrictEqual(sanObject, undefined);
+
+    const orbitalTransfers = calculateOrbitalTransfers(youObject, sanObject);
+
+    assert.strictEqual(orbitalTransfers, 4);
+}
+
+function puzzle2(orbitMap: Map<string, SpaceObject>): void {
+    const youObject = orbitMap.get('YOU') as SpaceObject;
+    const sanObject = orbitMap.get('SAN') as SpaceObject;
+
+    assert.notStrictEqual(youObject, undefined);
+    assert.notStrictEqual(sanObject, undefined);
+
+    const orbitalTransfers = calculateOrbitalTransfers(youObject, sanObject);
+
+    consola.info(`The minimum orbital transfers required to move from YOU to SAN are ${orbitalTransfers}`);
 }
 
 function readInputLines(): string[] {
@@ -45,32 +90,58 @@ function readInputLines(): string[] {
         .filter(line => !!line);
 }
 
+function calculateOrbitalTransfers(leftObject: SpaceObject, rightObject: SpaceObject): number {
+    const leftPath = leftObject.getPathToRootObject().map(spaceObject => spaceObject.getIdentifier());
+    const rightPath = rightObject.getPathToRootObject().map(spaceObject => spaceObject.getIdentifier());
+    const connectingIdentifier = leftPath.find(identifier => rightPath.includes(identifier));
+    const leftOrbitalTransfers = calculateOrbitalTransfersUntil(leftPath, connectingIdentifier as string);
+    const rightOrbitalTransfers = calculateOrbitalTransfersUntil(rightPath, connectingIdentifier as string);
+
+    return leftOrbitalTransfers + rightOrbitalTransfers;
+}
+
+function calculateOrbitalTransfersUntil(identifiers: string[], connectingIdentifier: string): number {
+    for (let i = 0; i < identifiers.length; i++) {
+        if (identifiers[i] === connectingIdentifier) {
+            return i - 1;
+        }
+    }
+
+    return 0;
+}
+
 function calculateOrbitCountChecksum(orbits: string[]): number {
-    const identifierToSpaceObject: Map<string, SpaceObject> = new Map<string, SpaceObject>();
+    const orbitMap: Map<string, SpaceObject> = createOrbitMap(orbits);
 
-    orbits.forEach(orbit => parseOrbit(identifierToSpaceObject, orbit));
-
-    const spaceObjects = Array.from(identifierToSpaceObject.values());
+    const spaceObjects = Array.from(orbitMap.values());
 
     return spaceObjects.reduce((orbitCountChecksum: number, spaceObject: SpaceObject) => orbitCountChecksum + spaceObject.countOrbits(), 0);
 }
 
-function parseOrbit(identifierToSpaceObject: Map<string, SpaceObject>, orbit: string): void {
+function createOrbitMap(orbits: string[]): Map<string, SpaceObject> {
+    const orbitMap: Map<string, SpaceObject> = new Map<string, SpaceObject>();
+
+    orbits.forEach(orbit => parseOrbit(orbitMap, orbit));
+
+    return orbitMap;
+}
+
+function parseOrbit(orbitMap: Map<string, SpaceObject>, orbit: string): void {
     if (!orbit) {
         return;
     }
 
     const [leftIdentifier, rightIdentifier] = orbit.split(')');
 
-    const leftObject = getSpaceObject(identifierToSpaceObject, leftIdentifier);
-    const rightObject = getSpaceObject(identifierToSpaceObject, rightIdentifier);
+    const leftObject = getSpaceObject(orbitMap, leftIdentifier);
+    const rightObject = getSpaceObject(orbitMap, rightIdentifier);
 
     leftObject.addChild(rightObject);
     rightObject.setParent(leftObject);
 }
 
-function getSpaceObject(identifierToSpaceObject: Map<string, SpaceObject>, identifier: string): SpaceObject {
-    const spaceObject = identifierToSpaceObject.get(identifier);
+function getSpaceObject(orbitMap: Map<string, SpaceObject>, identifier: string): SpaceObject {
+    const spaceObject = orbitMap.get(identifier);
 
     if (spaceObject) {
         return spaceObject;
@@ -78,7 +149,7 @@ function getSpaceObject(identifierToSpaceObject: Map<string, SpaceObject>, ident
 
     const newSpaceObject = new SpaceObject(identifier);
 
-    identifierToSpaceObject.set(identifier, newSpaceObject);
+    orbitMap.set(identifier, newSpaceObject);
 
     return newSpaceObject;
 }
@@ -93,6 +164,10 @@ class SpaceObject {
     ) {
     }
 
+    public getIdentifier(): string {
+        return this.identifier;
+    }
+
     public addChild(spaceObject: SpaceObject): void {
         this.children.push(spaceObject);
     }
@@ -102,13 +177,20 @@ class SpaceObject {
     }
 
     public countOrbits(): number {
+        return this.getPathToRootObject().length;
+    }
+
+    public getPathToRootObject(): SpaceObject[] {
         const {parent} = this;
 
         if (!parent) {
-            return 0;
+            return [];
         }
 
-        return 1 + parent.countOrbits();
+        return [
+            this,
+            ...parent.getPathToRootObject()
+        ];
     }
 
 }
