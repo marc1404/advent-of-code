73b4e2f31a79a29967a6017d9c2eee78c6bdcc99
Marc Vornetran <marc.vornetran@gmail.com>
2020-12-11 22:19:29 +0100

    Solve day 11 part 2

--- 2020/day11/day11.go
+++ 2020/day11/day11.go
@@ -7,43 +7,106 @@ import (
 )
 
 func main() {
+	testLayout := readAllLines("./day11/test_input.txt")
 	lines := readAllLines("./day11/input.txt")
 
 	log.Println("Day 11 Part 01")
-	testPartOne()
+	testPartOne(testLayout)
 	partOne(lines)
 
 	log.Println()
 
 	log.Println("Day 11 Part 02")
-	testPartTwo()
+	testPartTwo(testLayout)
 	partTwo(lines)
 }
 
-func testPartOne() {
-	layout := readAllLines("./day11/test_input.txt")
-
-	log.Println(countOccupiedWhenPredictionStabilized(layout))
+func testPartOne(layout [][]rune) {
+	log.Println(countOccupiedWhenPredictionStabilized(layout, countAdjacentOccupiedSeats, 4))
 }
 
 func partOne(layout [][]rune) {
-	log.Println(countOccupiedWhenPredictionStabilized(layout))
+	log.Println(countOccupiedWhenPredictionStabilized(layout, countAdjacentOccupiedSeats, 4))
 }
 
-func testPartTwo() {
+func testPartTwo(layout [][]rune) {
+	printLayout(layout)
 
+	tempLayout := [][]rune{
+		[]rune(".......#."),
+		[]rune("...#....."),
+		[]rune(".#......."),
+		[]rune("........."),
+		[]rune("..#L....#"),
+		[]rune("....#...."),
+		[]rune("........."),
+		[]rune("#........"),
+		[]rune("...#....."),
+	}
+
+	log.Println(countOccupiedSeatsLineOfSight(tempLayout, 4, 3))
+
+	tempLayout = [][]rune{
+		[]rune("............."),
+		[]rune(".L.L.#.#.#.#."),
+		[]rune("............."),
+	}
+
+	log.Println(countOccupiedSeatsLineOfSight(tempLayout, 1, 1))
+
+	tempLayout = [][]rune{
+		[]rune(".##.##."),
+		[]rune("#.#.#.#"),
+		[]rune("##...##"),
+		[]rune("...L..."),
+		[]rune("##...##"),
+		[]rune("#.#.#.#"),
+		[]rune(".##.##."),
+	}
+
+	log.Println(countOccupiedSeatsLineOfSight(tempLayout, 3, 3))
+
+	tempLayout = [][]rune{
+		[]rune("#.##.##.##"),
+		[]rune("#######.##"),
+		[]rune("#.#.#..#.."),
+		[]rune("####.##.##"),
+		[]rune("#.##.##.##"),
+		[]rune("#.#####.##"),
+		[]rune("..#.#....."),
+		[]rune("##########"),
+		[]rune("#.######.#"),
+		[]rune("#.#####.##"),
+	}
+
+	prediction, _ := predictSeatLayout(tempLayout, countOccupiedSeatsLineOfSight, 5)
+
+	printLayout(prediction)
+
+	log.Println(countOccupiedWhenPredictionStabilized(layout, countOccupiedSeatsLineOfSight, 5))
 }
 
-func partTwo(lines [][]rune) {
-
+func partTwo(layout [][]rune) {
+	log.Println(countOccupiedWhenPredictionStabilized(layout, countOccupiedSeatsLineOfSight, 5))
 }
 
-func countOccupiedWhenPredictionStabilized(layout [][]rune) int {
+var directions = [][]int{
+	{-1, -1},
+	{-1, 0},
+	{-1, 1},
+	{0, -1},
+	{0, 1},
+	{1, -1},
+	{1, 0},
+	{1, 1},
+}
+
+func countOccupiedWhenPredictionStabilized(layout [][]rune, countSurroundingOccupiedSeats CountSurroundingOccupiedSeats, leaveSeatThreshold int) int {
 	prediction := layout
 	var stable bool
 
 	for {
-		prediction, stable = predictSeatLayout(prediction)
+		prediction, stable = predictSeatLayout(prediction, countSurroundingOccupiedSeats, leaveSeatThreshold)
 
 		if stable {
 			break
@@ -53,7 +116,9 @@ func countOccupiedWhenPredictionStabilized(layout [][]rune) int {
 	return countOccupiedSeats(prediction)
 }
 
-func predictSeatLayout(layout [][]rune) ([][]rune, bool) {
+type CountSurroundingOccupiedSeats = func(layout [][]rune, y, x int) int
+
+func predictSeatLayout(layout [][]rune, countSurroundingOccupiedSeats CountSurroundingOccupiedSeats, leaveSeatThreshold int) ([][]rune, bool) {
 	prediction := make([][]rune, len(layout))
 	stable := true
 
@@ -63,14 +128,14 @@ func predictSeatLayout(layout [][]rune) ([][]rune, bool) {
 		for x, seatValue := range row {
 			seat := string(seatValue)
 			predictedSeat := seatValue
-			occupiedCount := countAdjacentOccupiedSeats(layout, y, x)
+			occupiedCount := countSurroundingOccupiedSeats(layout, y, x)
 
 			if seat == "L" && occupiedCount == 0 {
 				predictedSeat = []rune("#")[0]
 				stable = false
 			}
 
-			if seat == "#" && occupiedCount >= 4 {
+			if seat == "#" && occupiedCount >= leaveSeatThreshold {
 				predictedSeat = []rune("L")[0]
 				stable = false
 			}
@@ -84,20 +149,10 @@ func predictSeatLayout(layout [][]rune) ([][]rune, bool) {
 
 func countAdjacentOccupiedSeats(layout [][]rune, y, x int) int {
 	occupiedCount := 0
-	modifiers := [][]int{
-		{-1, -1},
-		{-1, 0},
-		{-1, 1},
-		{0, -1},
-		{0, 1},
-		{1, -1},
-		{1, 0},
-		{1, 1},
-	}
 
-	for _, modifier := range modifiers {
-		modY := modifier[0]
-		modX := modifier[1]
+	for _, direction := range directions {
+		modY := direction[0]
+		modX := direction[1]
 
 		if isSeatOccupied(layout, y+modY, x+modX) {
 			occupiedCount++
@@ -107,18 +162,50 @@ func countAdjacentOccupiedSeats(layout [][]rune, y, x int) int {
 	return occupiedCount
 }
 
-func isSeatOccupied(layout [][]rune, y, x int) bool {
+func countOccupiedSeatsLineOfSight(layout [][]rune, y, x int) int {
+	occupiedCount := 0
+
+	for _, direction := range directions {
+		distance := 1
+		modY := direction[0]
+		modX := direction[1]
+
+		for {
+			seat, outOfBounds := getSeat(layout, y+modY*distance, x+modX*distance)
+			distance++
+			isOccupied := seat == "#"
+
+			if isOccupied {
+				occupiedCount++
+			}
+
+			if outOfBounds || isOccupied || seat == "L" {
+				break
+			}
+		}
+	}
+
+	return occupiedCount
+}
+
+func getSeat(layout [][]rune, y, x int) (seat string, outOfBounds bool) {
 	if y < 0 || y >= len(layout) {
-		return false
+		return "", true
 	}
 
 	row := layout[y]
 
 	if x < 0 || x >= len(row) {
-		return false
+		return "", true
 	}
 
-	seat := string(row[x])
+	seat = string(row[x])
+
+	return seat, false
+}
+
+func isSeatOccupied(layout [][]rune, y, x int) bool {
+	seat, _ := getSeat(layout, y, x)
 
 	return seat == "#"
 }
@@ -135,6 +222,12 @@ func countOccupiedSeats(layout [][]rune) int {
 	}
 
 	return occupiedCount
+}
+
+func printLayout(layout [][]rune) {
+	for _, row := range layout {
+		log.Println(string(row))
+	}
 }
 
 func readAllLines(filePath string) [][]rune {
