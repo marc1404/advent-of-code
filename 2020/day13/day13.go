704414833e168967b8e9b42ab8ccd853e5bcb9b0
Marc Vornetran <marc.vornetran@gmail.com>
2020-12-13 23:57:13 +0100

    Prepare day 13

---
+++ 2020/day13/day13.go
@@ -0,0 +1,31 @@
+package main
+
+import "log"
+
+func main() {
+	log.Println("Day 13 Part 01")
+	testPartOne()
+	partOne()
+
+	log.Println()
+
+	log.Println("Day 13 Part 02")
+	testPartTwo()
+	partTwo()
+}
+
+func testPartOne() {
+
+}
+
+func partOne() {
+
+}
+
+func testPartTwo() {
+
+}
+
+func partTwo() {
+
+}
