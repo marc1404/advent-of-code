52408244e1caf4798797674a8eba555cb2d73b61
Marc Vornetran <marc.vornetran@gmail.com>
2020-12-14 18:50:23 +0100

    Solve day 14 part 2

--- 2020/day14/day14.go
+++ 2020/day14/day14.go
@@ -26,23 +26,30 @@ func main() {
 }
 
 func testPartOne(instructions []string) {
-	log.Println(initializeAndSumFerryDockingProgram(instructions))
+	log.Println(initializeAndSumFerryDockingProgram(instructions, writeToMemoryThroughMask))
 }
 
 func partOne(instructions []string) {
-	log.Println(initializeAndSumFerryDockingProgram(instructions))
+	log.Println(initializeAndSumFerryDockingProgram(instructions, writeToMemoryThroughMask))
 }
 
 func testPartTwo() {
+	instructions := []string{
+		"mask = 000000000000000000000000000000X1001X",
+		"mem[42] = 100",
+		"mask = 00000000000000000000000000000000X0XX",
+		"mem[26] = 1",
+	}
 
+	log.Println(initializeAndSumFerryDockingProgram(instructions, writeToMemoryWithFloatingBits))
 }
 
 func partTwo(instructions []string) {
-
+	log.Println(initializeAndSumFerryDockingProgram(instructions, writeToMemoryWithFloatingBits))
 }
 
-func initializeAndSumFerryDockingProgram(instructions []string) int {
-	memory := initializeFerryDockingProgram(instructions)
+func initializeAndSumFerryDockingProgram(instructions []string, writeToMemory WriteToMemory) int {
+	memory := initializeFerryDockingProgram(instructions, writeToMemory)
 	sum := 0
 
 	for _, value := range memory {
@@ -52,7 +59,9 @@ func initializeAndSumFerryDockingProgram(instructions []string) int {
 	return sum
 }
 
-func initializeFerryDockingProgram(instructions []string) map[int]int {
+type WriteToMemory func(target string, valueAsString string, mask string, memory map[int]int) map[int]int
+
+func initializeFerryDockingProgram(instructions []string, writeToMemory WriteToMemory) map[int]int {
 	memory := make(map[int]int)
 	var mask string
 
@@ -63,7 +72,7 @@ func initializeFerryDockingProgram(instructions []string) map[int]int {
 		case "mask":
 			mask = value
 		default:
-			memory = writeToMemoryThroughMask(target, value, mask, memory)
+			memory = writeToMemory(target, value, mask, memory)
 		}
 	}
 
@@ -81,8 +90,7 @@ func parseInstruction(instruction string) (target, value string) {
 func writeToMemoryThroughMask(target string, valueAsString string, mask string, memory map[int]int) map[int]int {
 	memoryAddress := parseMemoryAddress(target)
 	value, _ := strconv.Atoi(valueAsString)
-	valueAsBinary := strconv.FormatInt(int64(value), 2)
-	valueAsBinary = fmt.Sprintf("%036v", valueAsBinary)
+	valueAsBinary := intToBinary(value)
 	resultBits := make([]string, 36)
 
 	for i := 0; i < 36; i++ {
@@ -98,6 +106,45 @@ func writeToMemoryThroughMask(target string, valueAsString string, mask string, 
 	return memory
 }
 
+func writeToMemoryWithFloatingBits(target string, valueAsString string, mask string, memory map[int]int) map[int]int {
+	value, _ := strconv.Atoi(valueAsString)
+	memoryAddress := parseMemoryAddress(target)
+	memoryAsBinary := intToBinary(memoryAddress)
+
+	for i := 0; i < 36; i++ {
+		bit := string(memoryAsBinary[i])
+		maskBit := string(mask[i])
+		newBit := applyMaskWithFloatingBits(bit, maskBit)
+		memoryAsBinary = replaceAtIndex(memoryAsBinary, []rune(newBit)[0], i)
+	}
+
+	permutations := getMemoryAddressPermutations(memoryAsBinary, []string{})
+
+	for _, permutation := range permutations {
+		address, _ := strconv.ParseInt(permutation, 2, 64)
+		memory[int(address)] = value
+	}
+
+	return memory
+}
+
+func getMemoryAddressPermutations(memoryAsBinary string, permutations []string) []string {
+	index := strings.Index(memoryAsBinary, "X")
+
+	if index == -1 {
+		permutations = append(permutations, memoryAsBinary)
+		return permutations
+	}
+
+	zeroVariant := replaceAtIndex(memoryAsBinary, '0', index)
+	oneVariant := replaceAtIndex(memoryAsBinary, '1', index)
+
+	permutations = getMemoryAddressPermutations(zeroVariant, permutations)
+	permutations = getMemoryAddressPermutations(oneVariant, permutations)
+
+	return permutations
+}
+
 func applyMask(bit string, mask string) string {
 	switch mask {
 	case "0":
@@ -111,6 +158,19 @@ func applyMask(bit string, mask string) string {
 	}
 }
 
+func applyMaskWithFloatingBits(bit string, mask string) string {
+	switch mask {
+	case "0":
+		return bit
+	case "1":
+		return "1"
+	case "X":
+		return "X"
+	default:
+		panic(fmt.Sprintf("Unexpected mask: %v!", mask))
+	}
+}
+
 func parseMemoryAddress(target string) int {
 	pattern := regexp.MustCompile("mem\\[([0-9]+)]")
 	matches := pattern.FindAllStringSubmatch(target, -1)
@@ -118,6 +178,19 @@ func parseMemoryAddress(target string) int {
 	memoryAddress, _ := strconv.Atoi(match[1])
 
 	return memoryAddress
+}
+
+func intToBinary(value int) string {
+	binary := strconv.FormatInt(int64(value), 2)
+
+	return fmt.Sprintf("%036v", binary)
+}
+
+func replaceAtIndex(in string, r rune, i int) string {
+	out := []rune(in)
+	out[i] = r
+
+	return string(out)
 }
 
 func readAllLines(filePath string) (lines []string) {
