56afaa2a7d7bc08527c81da5bdab92ea012b136f
Marc Vornetran <marc.vornetran@gmail.com>
2020-12-29 15:55:51 +0100

    Prepare day 23-25

---
+++ 2020/day24/day24.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+func main() {
+	log.Println("Day 24 Part 01")
+	testPartOne()
+	partOne()
+
+	log.Println()
+
+	log.Println("Day 24 Part 02")
+	testPartTwo()
+	partTwo()
+}
+
+func testPartOne() {
+}
+
+func partOne() {
+}
+
+func testPartTwo() {
+}
+
+func partTwo() {
+}
+
+func readAllLines(filePath string) (lines []string) {
+	file, err := os.Open(filePath)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if line == "" {
+			continue
+		}
+
+		lines = append(lines, line)
+	}
+
+	return lines
+}
